bc218c45c69c76f469195df81ba41ac59d2204d3
NikosMouzakitis <mzktsn@gmail.com>
2020-02-27 15:31:24 +0100

    Update uuarts.sh

--- uuarts.sh
+++ uuarts.sh
@@ -1,3 +1,6 @@
+### specific for my laptop. should use your serial USB ports by path.
+
+
 
 #xterm -geometry 60x120+0+30  -fa Monaco -fs 12 -bg black -fg orange -e "minicom -D /dev/serial/by-path/pci-0000:00:14.0-usb-0:3:1.0" &
 #xterm -geometry 60x120+674+30  -fa Monaco -fs 12 -bg  black -fg orange  -e "minicom -D /dev/serial/by-path/pci-0000:00:14.0-usb-0:1:1.0" &
