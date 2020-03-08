113209511ff0cdebecf56aaed1a5280de620afda
NikosMouzakitis <mzktsn@gmail.com>
2020-03-08 04:37:02 +0100

    Update pass_reference_to_register.c

--- pass_reference_to_register.c
+++ pass_reference_to_register.c
@@ -6,6 +6,6 @@ int main(int argc, char *argv[])
 	printf("addr: %x\n",(unsigned int) &buf);	
 
 	asm("mov %0 , %%ebx" ::"g"(&buf));
-	//disassembling we observer %ebx containing the address of the buf.
+	//disassembling we observe %ebx containing the address of the buf.
 	return(0);
 }
